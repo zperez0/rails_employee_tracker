bf7c6efcf4a5fc5ad1e71fd671935b47be96b7cf
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 14:54:55 -0700

    update tests

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 describe Department do
   it { should have_many :employees }
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+  it { should have_and_belong_to_many :projects }
+end
+
+describe Department do
+  it("titleizes the name of an department") do
+    department = Department.create({name: "Tech"})
+    expect(department.name).to eq "Tech"
+  end
 end
