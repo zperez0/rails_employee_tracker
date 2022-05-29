1d79ebc88cb61fd556d7ee7bb9efbc93ccad7826
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 12:31:47 -0700

    create employee table

---
+++ db/migrate/20220529191909_create_employees.rb
@@ -0,0 +1,10 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :job_title
+      
+      t.timestamps
+    end
+  end
+end
