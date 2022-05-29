0a9dd5e23e1b5d5b6ea1476d92c0cbab427a8d11
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 12:44:23 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_191909) do
+ActiveRecord::Schema.define(version: 2022_05_29_193552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 2022_05_29_191909) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "job_title"
+    t.integer "department_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "employees", "departments"
 end
