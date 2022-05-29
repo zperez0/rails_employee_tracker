fd41eb50c890ef8cde572f82cb6eda575c383820
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 14:16:06 -0700

    add projects and join tables

---
+++ db/migrate/20220529211007_add_projects_and_join_table.rb
@@ -0,0 +1,14 @@
+class AddProjectsAndJoinTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :title
+
+      t.timestamps
+  end
+
+  create_table :department_projects, id: false do |t|
+    t.belongs_to :project, index: true
+    t.belongs_to :department, index: true
+    end
+  end
+end
