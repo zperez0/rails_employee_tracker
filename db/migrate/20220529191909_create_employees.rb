7648059f5dc191a3a305bb926d200ee8a8bebb86
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 12:43:41 -0700

    update employee table to include department foreign key

--- db/migrate/20220529191909_create_employees.rb
+++ db/migrate/20220529191909_create_employees.rb
@@ -3,7 +3,8 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
     create_table :employees do |t|
       t.string :name
       t.string :job_title
-      
+      t.integer :department_id
+
       t.timestamps
     end
   end
