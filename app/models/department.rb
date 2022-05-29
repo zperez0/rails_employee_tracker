893931eb1a079dc541949eb5b4fa5991cfb9b4bc
Yazmin Perez <zoeperez333@gmail.com>
2022-05-29 14:26:47 -0700

    add department class

---
+++ app/models/department.rb
@@ -0,0 +1,12 @@
+class Department < ApplicationRecord
+  has_and_belongs_to_many :projects
+  has_many :employees, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
+  before_save :titleize_department
+
+  private
+  def titleize_department
+    self.name = self.name.titleize
+  end
+end
