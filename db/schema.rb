7bafeebaf6da07ef3071e956ad9bfbab2a77fa0e
Yazmin Perez <zoeperez333@gmail.com>
2022-05-31 21:21:12 -0700

    update schema. drop departments table. remove employees departments key

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_032915) do
+ActiveRecord::Schema.define(version: 2022_06_01_035838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "departments", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -40,5 +34,4 @@ ActiveRecord::Schema.define(version: 2022_06_01_032915) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "employees", "departments"
 end
